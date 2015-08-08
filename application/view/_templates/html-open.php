2caa3366f4c953c60b92c570030f10bcf7b412e1
Stephanie Marie Castilla <stephanie.m.castilla@gmail.com>
2015-08-08 00:23:25 -0400

    Fixed Mobile Icon

--- application/view/_templates/html-open.php
+++ application/view/_templates/html-open.php
@@ -6,7 +6,7 @@
 <meta content="" name="author"/>
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo URL; ?>public/img/rab_icon.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo Config::get('URL'); ?>public/img/rab_icon.png">
     <!-- META -->
     <meta charset="utf-8">
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
